9b293feadfc6095f7a52d852306c9f5f87778407
Alexandru Muresanu <alexandru.gabriel.muresanu@gmail.com>
2022-04-12 15:17:01 -0400

    Revert "Revert "last mehods""
    This reverts commit 27cbd2d1cc6249a8140cb095fd6282fec5dfcc5a.

--- src/main/java/com/mycompany/vendingmachine/ui/VendingMachineView.java
+++ src/main/java/com/mycompany/vendingmachine/ui/VendingMachineView.java
@@ -1,10 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.vendingmachine.ui;
 
-import com.mycompany.classroster.ui.UserIO;
+package com.mycompany.vendingmachine.ui;
 import com.mycompany.vendingmachine.dto.Item;
 import java.util.List;
 
