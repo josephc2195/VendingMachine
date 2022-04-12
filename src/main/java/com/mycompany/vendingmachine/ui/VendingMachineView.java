24004511485a80fab3840d1b769baf8fbc5cf2a9
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2022-04-12 11:38:52 -0400

    Update VendingMachineView.java
    added some methods to the view

--- src/main/java/com/mycompany/vendingmachine/ui/VendingMachineView.java
+++ src/main/java/com/mycompany/vendingmachine/ui/VendingMachineView.java
@@ -4,10 +4,52 @@
  */
 package com.mycompany.vendingmachine.ui;
 
+import com.mycompany.classroster.ui.UserIO;
+import com.mycompany.vendingmachine.dto.Item;
+import java.util.List;
+
 /**
  *
  * @author chica
  */
 public class VendingMachineView {
+    private UserIO io;
+    
+    public VendingMachineView(UserIO io) {
+        this.io = io;
+    }
+    
+    public int printMenuAndGetSelection() {
+        io.print("Main Menu");
+        io.print("1. List items");
+        io.print("2. Choose an item");
+        io.print("3. Add money");
+        io.print("4. Return money");
+        io.print("5. Add item");
+        io.print("6. Exit");
+        
+        return io.readInt("Please select from the above choices.", 1, 6);
+    } 
+    
+    public void displayItemList(List<Item> items) {
+        for(Item i : items) { 
+            displayItem(i);
+            io.print("\n");
+        }
+    }
+    
+    public void displayItem(Item item) {
+        if(item != null) {
+            io.print("Item name: " + item.getName());
+            io.print("Item price: $" + item.getPrice());
+            io.print("Item quantity: " + item.getQuantity());
+        }
+    }
+    
+    public void addedMoneyBanner(double amount) {
+        io.print("Added $" + amount);
+    }
+    
+    
     
 }
