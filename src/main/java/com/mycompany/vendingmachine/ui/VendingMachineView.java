5007ab8ad19eed4055802444acc121478ad9f71a
Joseph Chica <54424231+josephc2195@users.noreply.github.com>
2022-04-12 13:21:28 -0400

    Update VendingMachineView.java

--- src/main/java/com/mycompany/vendingmachine/ui/VendingMachineView.java
+++ src/main/java/com/mycompany/vendingmachine/ui/VendingMachineView.java
@@ -19,6 +19,11 @@ public class VendingMachineView {
         this.io = io;
     }
     
+    /**
+     * displays menu and gets user input
+     * 
+     * @return user selected menu option
+     */
     public int printMenuAndGetSelection() {
         io.print("Main Menu");
         io.print("1. List items");
@@ -31,6 +36,11 @@ public class VendingMachineView {
         return io.readInt("Please select from the above choices.", 1, 6);
     } 
     
+    /**
+     * This method displays all current dvds in collection
+     * 
+     * @param dvdList list of dvds
+     */
     public void displayItemList(List<Item> items) {
         for(Item i : items) { 
             displayItem(i);
@@ -38,6 +48,11 @@ public class VendingMachineView {
         }
     }
     
+    /**
+     * Displays all information for selected DVD
+     * 
+     * @param dvd object
+     */
     public void displayItem(Item item) {
         if(item != null) {
             io.print("Item name: " + item.getName());
@@ -46,10 +61,40 @@ public class VendingMachineView {
         }
     }
     
+    /**
+     * Displays a successful banner that amount has been added
+     * @param amount - amount being added
+     */
     public void addedMoneyBanner(double amount) {
-        io.print("Added $" + amount);
+        if(amount > 0 ) {
+            io.print("Added $" + amount);
+        }
     }
     
+    public void returnMoneyBanner(double amount) {
+        io.print("Returning $" + amount);
+    }
     
+    /**
+     * This method asks the user what item they want
+     * 
+     * @return String the item the user selects
+     */
+    public String chooseItem() {
+        return io.readString("Which item would you like? ");
+    }
     
+    /**
+     * This method get the information from the user about the new item being added
+     * 
+     * @return Item - item object that is being added to our collection
+     */
+    public Item addNewItem() {
+        Item addedItem = new Item();
+        addedItem.setName(io.readString("Please enter the name of item: "));
+        addedItem.setPrice(io.readDouble("Please enter the price of item: "));
+        addedItem.setQuantity(io.readInt("Please enter the quantity of item: "));
+        
+        return addedItem;
+    }
 }
